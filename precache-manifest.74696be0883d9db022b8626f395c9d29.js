840cc1d06fe66769436eec67dc3556573d6df779
John Unwin <john@fingertip.co.nz>
2019-10-02 22:39:15 +1300

    Updates

---
+++ precache-manifest.74696be0883d9db022b8626f395c9d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "694fde4f7cbc75ab631a3062db5f4089",
+    "url": "/static/media/magnifying-glass.694fde4f.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "202cee4eaa77c737b4c5",
+    "url": "/static/js/main.0a77fadf.chunk.js"
+  },
+  {
+    "revision": "2e84a1506dfc43fc9826",
+    "url": "/static/js/2.09415353.chunk.js"
+  },
+  {
+    "revision": "202cee4eaa77c737b4c5",
+    "url": "/static/css/main.66d1d06e.chunk.css"
+  },
+  {
+    "revision": "064632218749257bce8a7342eddc83f5",
+    "url": "/index.html"
+  }
+];
